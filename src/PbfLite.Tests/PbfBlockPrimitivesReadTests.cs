9d0eebf0b007f296c47491f36fc50f6df63a5741
Lukas Kabrt <lukas@kabrt.cz>
2018-01-31 21:43:40 +0100

    Added functions for reading system types (#1)
    * Implemented reading of several system types
    
    * Implemented tests for reading system types
    
    * Added additional unit tests

--- src/PbfLite.Tests/PbfBlockPrimitivesReadTests.cs
+++ src/PbfLite.Tests/PbfBlockPrimitivesReadTests.cs
@@ -4,41 +4,6 @@ using Xunit;
 namespace PbfLite.Tests {
     public class PbfBlockPrimitivesReadTests {
         [Theory]
-        [InlineData(new byte[] { 0x08 }, 1)]
-        [InlineData(new byte[] { 0x78 }, 15)]
-        [InlineData(new byte[] { 0x80, 0x01 }, 16)]
-        public void ReadFieldHeader_ReadsFieldNumbers(byte[] data, int fieldNumber) {
-            var block = PbfBlock.Create(data);
-
-            var header = block.ReadFieldHeader();
-
-            Assert.Equal(fieldNumber, header.fieldNumber);
-        }
-
-        [Theory]
-        [InlineData(new byte[] { 0x08 }, WireType.Variant)]
-        [InlineData(new byte[] { 0x09 }, WireType.Fixed64)]
-        [InlineData(new byte[] { 0x0A }, WireType.String)]
-        [InlineData(new byte[] { 0x0D }, WireType.Fixed32)]
-        public void ReadFieldHeader_ReadsWireTypes(byte[] data, WireType wireType) {
-            var block = PbfBlock.Create(data);
-
-            var header = block.ReadFieldHeader();
-
-            Assert.Equal(wireType, header.wireType);
-        }
-
-        [Fact]
-        public void ReadFieldHeader_ReturnsNoneWhenEndOfBlockIsReached() {
-            var block = PbfBlock.Create(new byte[] { });
-
-            var header = block.ReadFieldHeader();
-
-            Assert.Equal(0, header.fieldNumber);
-            Assert.Equal(WireType.None, header.wireType);
-        }
-
-        [Theory]
         [InlineData(new byte[] { 0x00, 0x00, 0x00, 0x00 }, 0)]
         [InlineData(new byte[] { 0x01, 0x00, 0x00, 0x00 }, 1)]
         [InlineData(new byte[] { 0xFF, 0x00, 0x00, 0x00 }, 255)]
@@ -111,34 +76,6 @@ namespace PbfLite.Tests {
             var block = PbfBlock.Create(data);
 
             var number = block.ReadVarint64();
-
-            Assert.Equal(expectedNumber, number);
-        }
-
-        [Theory]
-        [InlineData(0, 0)]
-        [InlineData(1, -1)]
-        [InlineData(2, 1)]
-        [InlineData(4294967294, 2147483647)]
-        [InlineData(4294967295, -2147483648)]
-        private void Zag_Decodes32BitZiggedValues(uint encodedNumber, int expectedNumber) {
-            var block = PbfBlock.Create(new byte[] { });
-
-            var number = block.Zag(encodedNumber);
-
-            Assert.Equal(expectedNumber, number);
-        }
-
-        [Theory]
-        [InlineData(0, 0)]
-        [InlineData(1, -1)]
-        [InlineData(2, 1)]
-        [InlineData(18446744073709551614UL, 9223372036854775807L)]
-        [InlineData(18446744073709551615UL, -9223372036854775808L)]
-        private void Zag_Decodes64BitZiggedValues(ulong encodedNumber, long expectedNumber) {
-            var block = PbfBlock.Create(new byte[] { });
-
-            var number = block.Zag(encodedNumber);
 
             Assert.Equal(expectedNumber, number);
         }
