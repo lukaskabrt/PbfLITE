97313b292caaf34e73f01b4246de684c2015a916
Lukas Kabrt <lukas@kabrt.cz>
2025-10-04 21:05:59 +0200

    Performance improvements
    * Use private fields instead of properties.

---
+++ src/PbfLite.Benchmark/ReadPrimitivesBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PbfLite.Benchmark;
+
+public class ReadPrimitivesBenchmarks
+{
+    private static readonly byte[] Fixed32Data = new byte[] { 0x01, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF };
+
+    [Benchmark]
+    public uint ReadFixed32()
+    {
+        var block = PbfBlock.Create(Fixed32Data);
+        block.ReadFixed32();
+        block.ReadFixed32();
+        block.ReadFixed32();
+        block.ReadFixed32();
+        block.ReadFixed32();
+        return block.ReadFixed32();
+    }
+}
